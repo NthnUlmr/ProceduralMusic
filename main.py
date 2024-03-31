83989c76cbbe4a6e1cefd57cfeb1f27940958a82
Nathan <quaz990@gmail.com>
2024-03-30 21:12:33 -0700

    Some small fixes and improvements - I think I'll need to rework a lot of this if I want it to sound good

--- main.py
+++ main.py
@@ -34,6 +34,9 @@
 # \brief A script for experimenting with procedural music generation in python. At the moment it generates a song as a
 # piano melody in C major with corresponding harmony being a major tritone using an octave offset of the current melody note
 # as the root. The melody tries to force itself to walk back towards middle c.
+## \TODO The main problem with this at the moment is that there is no repitition. Repitition legitimizes. I think what this
+# algorithm needs is to generate a few measures of melody and then to repeat and add variations on top. This sounds almost
+# ideal for a genetic algorithm. The problem is how do you measure fitness???
 
 import rtmidi
 import time
@@ -42,7 +45,7 @@ import math
 import threading
 
 Instruments = {
-    "Piano":0,
+    "Piano":89,
 }
 def main():
     midiout = rtmidi.MidiOut()
@@ -53,7 +56,7 @@ def main():
     else:
         midiout.open_virtual_port("My virtual output")
 
-    random.seed(0)
+    random.seed(69)
 
     root = 60#random.choice(range(60,72))
 
@@ -62,32 +65,39 @@ def main():
 
     with midiout:
         instrument = random.choice(list(Instruments.values()))
-        melodyNote = Chord(midiout, [Note(root+16, 112)], instrument)
-        bpm = random.gauss(90.0,30.0)
-        baseBeat = random.choice([1.0,1.0/2.0,1.0/4.0,1.0/8.0])
+        melodyNote = Chord(midiout, [Note(root+12, 112)], 0)
+        bpm = random.gauss(140.0,30.0)
+        baseBeat = 1.0/4.0#random.choice([1.0,1.0/2.0,1.0/4.0,1.0/8.0])
         notesPerMeasure = random.randint(1,8)
-        volume = 70
+        volume = 50
+        chordThread = threading.Thread(target = print, args=("lmao",))
+        melodyThread = threading.Thread(target = print, args=("lmao",))
         while(count < maxLoops):
-            volume = random.gauss(volume, 2)
+            volume = random.gauss(50, 10)
             volume = min(max(volume,40),150)
-            isMajor = True#random.choice([True,False])
+            isMajor = random.choice([True,False])
 
             generate_nextMelody_note(midiout, melodyNote)
-            melodyNote.play(random.choice([1.0, 1.0 / 2.0, 1.0 / 4.0, 1.0 / 8.0]))
+            #melodyNote.play(random.choice([1.0, 1.0 / 2.0, 1.0 / 4.0, 1.0 / 8.0]))
 
-            root = melodyNote.notes[0].key + random.choice([0]) + random.choice([-2,-1,-1,-1,-1,0])*12
+            root = melodyNote.notes[0].key + random.choice([0]) + random.choice([-2, -2, -1])*12
             if(isMajor):
                 currentChord = generate_major_tritone(midiout, root, volume, instrument)
             else:
                 currentChord = generate_minor_tritone(midiout, root, volume, instrument)
-            chordDuration  = random.choice([4.0,3.0,2.0,1.5,1.0,1.0/2.0,1.0/4.0])
-            chordThread = threading.Thread(target = currentChord.play, args=(chordDuration,))
-            chordThread.start()
+            melodyDuration = random.choice([2.0, 1.5, 1.0, 1.0 / 2.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 8.0, 1.0 / 16.0]) * 60.0 / bpm / baseBeat
+            chordDuration  = random.choice([4.0,3.0,2.0,1.5,1.0,1.0/2.0,1.0/4.0])*60.0/bpm / baseBeat
 
-            melodyDuration = random.choice([2.0,1.5,1.0, 1.0 / 2.0, 1.0 / 4.0, 1.0 / 8.0, 1.0 / 16.0])
-            melodyThread = threading.Thread(target = melodyNote.play, args=(melodyDuration,))
-            melodyThread.start()
-            melodyThread.join()
+            if not (chordThread.is_alive()):
+                chordThread = chordThread = threading.Thread(target = currentChord.play, args=(chordDuration,))
+                chordThread.start()
+
+            if not (melodyThread.is_alive()):
+                melodyThread = threading.Thread(target = melodyNote.play, args=(melodyDuration,))
+                melodyThread.start()
+            #melodyThread.join()
+            #chordThread.join()
+            time.sleep(min(chordDuration,melodyDuration))
             count = count + 1
 
 
@@ -114,13 +124,14 @@ def generate_minor_tritone(midiout, root, volume, instrument):
 
 def generate_nextMelody_note(midiout, currentNote):
     prevNote = currentNote.notes[0].key
-    newNote = math.floor(random.gauss(currentNote.notes[0].key,7))
+    dir_to_middle_c = -1 * math.copysign(1, prevNote - 60)
+    newNote = currentNote.notes[0].key + abs(math.floor(random.gauss(0.0, 4))) * dir_to_middle_c
 
     octaveRoot = math.floor(newNote/12) * 12
     #key root + 0, 2, 4, 5, 7, 9, 10, 11, 12 # force the melody to stay in c major
-    dir_to_middle_c = -1*math.copysign(1,prevNote - 60)
+
     while(not( (newNote - octaveRoot) in [0,2,4,5,7,9,10,11,12])):
-        newNote = currentNote.notes[0].key + abs(math.floor(random.gauss(0.0, 7))) * dir_to_middle_c
+        newNote = currentNote.notes[0].key + abs(math.floor(random.gauss(0.0, 4))) * dir_to_middle_c
         octaveRoot = math.floor(newNote / 12) * 12
 
 
@@ -158,6 +169,14 @@ class Chord:
             self.midiout.send_message(play_msg)
 
 
+class ChordSequence:
+    def __init__(self, midiout,chords=[], delays=[]):
+        self.chords = chords
+        self.delays = delays
+
+    def play(self):
+        for ii in range(len(self.chords)):
+            self.chords[ii].play(self.delays[ii])
 
 
 if __name__ == "__main__":
